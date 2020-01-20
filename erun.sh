7058558d547fa7746564e3266c5ce912a0fae08c
hcc <hcc@hust.edu.cn>
2020-01-21 10:40:58 +1100

    fix

--- erun.sh
+++ erun.sh
@@ -1,4 +1,4 @@
 python pose_estimation/valid.py \
     --cfg experiments/mpii/resnet50/1_20_1.yaml \
     --flip-test \
-    --model-file output/mpii/pose_resnet_50/1_20_1.yaml/model_best.pth.tar
+    --model-file output/mpii/pose_resnet_50/1_20_1/model_best.pth.tar
